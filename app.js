f9718a7dfbeb1d7aa7b80ebb66bd4002248dd12d
shivam kushwah <97247055+Shivamkus@users.noreply.github.com>
2024-09-29 22:52:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
     
-}).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
 
 // API Routes
 const postRoutes = require('./routes/post');

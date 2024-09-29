110685c124ac3333fa334909f5ef3f0983f25731
shivam kushwah <97247055+Shivamkus@users.noreply.github.com>
2024-09-29 23:03:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
     
 

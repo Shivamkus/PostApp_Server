858f59b59f9d4ac4b7351ccfee4d9de7db813b5c
shivam kushwah <97247055+Shivamkus@users.noreply.github.com>
2024-09-29 22:49:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected!'))
+.catch(err => console.error('MongoDB connection error:', err));
+    
 }).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
 
 // API Routes
